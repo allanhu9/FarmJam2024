ff8e16d8dcf0e7ca6489f833dca8a28f92c6fb97
trooper4907 <wang.matthew12345@gmail.com>
2024-02-03 19:21:54 -0800

    Basic Day Night Cycle
    Added the following
    -Basic Day Night Cycle Script

---
+++ Assets/DayNightScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+
+namespace DayNightScript
+{
+    [RequireComponent(typeof(Light2D))]
+    public class DayNightScript : MonoBehaviour
+    {
+        public float duration = 5f;
+
+        [SerializeField] private Gradient time;
+        private Light2D light;
+        private float startTime;
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            light = GetComponent<Light2D>();
+            startTime = Time.time;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            float timeElapsed = Time.time - startTime;
+            float percent = Mathf.Sin(f: timeElapsed / duration * Mathf.PI * 2) * 0.5f + 0.5f;
+            percent = Mathf.Clamp01(percent);
+
+            light.color = time.Evaluate(percent);
+
+        }
+    }
+}
+
