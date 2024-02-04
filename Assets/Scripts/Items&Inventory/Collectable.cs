7960accf238ac2704db41a684ccbf505af58c561
Allan <allanhu9@gmail.com>
2024-02-03 23:51:08 -0800

    Added Another Test Item and Made Inventory Work

--- Assets/Scripts/Items&Inventory/Collectable.cs
+++ Assets/Scripts/Items&Inventory/Collectable.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Item))]
 public class Collectable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player player = other.GetComponent<Player>();
+        if (player) {
+            player.inventory.Add(GetComponent<Item>());
+            Destroy(gameObject);
+        }
     }
 }
